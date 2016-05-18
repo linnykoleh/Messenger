665270303a429d0876cb9778306ad3eddc0bf128
LinnykOleg <linnik.oleg.93@gmail.com>
2016-05-18 19:01:37 +0300

    provided uri parameters by @PathParam

--- src/main/java/com/messenger/resources/MessageResource.java
+++ src/main/java/com/messenger/resources/MessageResource.java
@@ -6,6 +6,7 @@ import com.messenger.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -24,8 +25,29 @@ public class MessageResource {
 */
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML) /*format of response*/
     public List<Message> getMessages(){
         return service.getAllMessages();
     }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long messageId){
+        /*
+             http://localhost:8080/messenger/webapi/messages/5
+
+             messageId = 5
+
+             Аннотация @PathParam нужна для получения парамера как messageId
+             На момент получения мессаджа мы можем не знать id мессаджа
+
+             Jersey передаст значение messageId и с указанным типом в аргументе метода если
+             аргумент будет аннотирован аннотацией @PathParam("messageId")
+        */
+
+        return service.getMessage(messageId);
+    }
+
+
 }
