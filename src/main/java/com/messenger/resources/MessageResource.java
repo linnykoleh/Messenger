70b8e5520d3514f7161a1b21c2f93a3fdb262be6
LinnykOleh <linnik.oleg.93@gmail.com>
2016-06-19 16:04:52 +0300

    post - create new message, put - update message by id, delete - remove by message id

--- src/main/java/com/messenger/resources/MessageResource.java
+++ src/main/java/com/messenger/resources/MessageResource.java
@@ -9,17 +9,11 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON) /* может быть аннотирован как класс так и метод */  /*ожидаем получить JSON Должен быть добавлен header Content-Type=application/json*/
+@Produces(MediaType.APPLICATION_JSON) /* может быть аннотирован как класс так и метод */
 public class MessageResource {
 
     final MessageService service = new MessageService();
-
-/*
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)  return format of response
-    public String getMessages(){
-        return "Hello world!";
-    }
-*/
 
     @GET
     @Produces(MediaType.APPLICATION_JSON) /*format of response*/
@@ -27,9 +21,8 @@ public class MessageResource {
         return service.getAllMessages();
     }
 
-    @GET
+    @GET /*read resources*/
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long messageId){
         /*
              http://localhost:8080/messenger/webapi/messages/5
@@ -46,12 +39,22 @@ public class MessageResource {
         return service.getMessage(messageId);
     }
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON) /*ожидаем получить JSON Должен быть добавлен header Content-Type=application/json*/
-    @Produces(MediaType.APPLICATION_JSON)
+    @POST /*creating new resources*/
     public Message addMessage(Message message){
         return service.addMessage(message);
     }
 
+    @PUT /*updating resources*/
+    @Path("/{messageId}")
+    public Message updateMessage(@PathParam("messageId") long messageId, Message message){
+        message.setId(messageId);
+        return service.updateMessage(message);
+    }
+
+    @DELETE /*deleting resources*/
+    @Path("/{messageId}")
+    public void deleteMessage(@PathParam("messageId") long messageId){
+        service.removeMessage(messageId);
+    }
 
 }
